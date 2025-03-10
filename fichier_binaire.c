bfa0c763b82aecbe6e7ffdfd4b917aa5d327f25c
Maulaz Tony <tony.maulaz@heig-vd.ch>
2025-03-10 14:03:01 +0100

    Add binary file

--- fichier_binaire.c
+++ fichier_binaire.c
@@ -15,6 +15,47 @@ int main(int argc, char *argv[])
     if( f == NULL )
         return ERROR_FILE;
 
+    // ecriture d'un tableau dans un fichier binaire
+    int c[] = {12, 345};
+    const size_t nb = sizeof(c) / sizeof(c[0]);
+    fwrite(c, sizeof(int), nb, f);
+
+    char* txt = "Hello";
+    fwrite(txt, sizeof(char), 5, f); // identique
+    fprintf(f, "%s", txt);
     fclose(f);
+
+    f = fopen("fichier", "r");
+    if( f == NULL )
+        return ERROR_FILE;
+
+    // lecture d'un tableau d'entier
+    // attention il faut impérativement donner la taille
+    int tab[2];
+    size_t nb_read = fread(tab, sizeof(int), 2, f);
+    if( nb_read == 2 ){
+        printf("Val 1 : %d  Val 2 : %d", tab[0], tab[1]);
+    }
+    fclose(f);
+
+    // structure
+    // grâce à fwrite, il est possible d'écrire et lire directement
+    // une structure.
+    Measure m = {.id=1, .value=12.3};
+    f = fopen("fichier", "w");
+    if( f == NULL )
+        return ERROR_FILE;
+    fwrite(&m, sizeof(Measure), 1, f);
+    fclose(f);
+
+    Measure mr;
+    f = fopen("fichier", "r");
+    if( f == NULL )
+        return ERROR_FILE;
+    fread(&mr, sizeof(Measure), 1, f);
+    fclose(f);
+
+    printf("\nid : %d   -  val : %lf", mr.id, mr.value);
+
     return 0;
 }
